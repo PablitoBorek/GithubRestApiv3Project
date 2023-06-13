ffee1dc2b113c59d2db0be7f393c31178759f09d
Paweł Borkowski <pawel.borkowski@hycom.pl>
2023-06-13 22:24:42 +0200

    Initial commit

---
+++ src/main/java/pawel/borkowski/GithubRestApiv3Project/GithubRestApiv3ProjectApplication.java
@@ -0,0 +1,13 @@
+package pawel.borkowski.GithubRestApiv3Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRestApiv3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRestApiv3ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pawel/borkowski/GithubRestApiv3Project/GithubRestApiv3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pawel.borkowski.GithubRestApiv3Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRestApiv3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
